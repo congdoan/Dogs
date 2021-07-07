166ca3dbecfcfa84170d9626930f2e8bc09a3c14
Cong Doan <cong.doan@gmail.com>
2021-07-07 07:32:04 +0700

    Implement the DogListAdapter, a subclass of RecyclerView.Adapter<DogListAdapter.DogViewHolder> where DogListAdapter.DogViewHolder is a subclass of RecyclerView.ViewHolder.

--- app/src/main/java/com/cdoan/dogs/model/Model.kt
+++ app/src/main/java/com/cdoan/dogs/model/Model.kt
@@ -3,7 +3,7 @@ package com.cdoan.dogs.model
 data class DogBreed(
     val breedId: String?,
     val dogBreed: String?,
-    val lifeSpan: String?,
+    val lifespan: String?,
     val breedGroup: String?,
     val bredFor: String?,
     val temperament: String?,
---
+++ app/src/main/java/com/cdoan/dogs/view/DogListAdapter.kt
@@ -0,0 +1,34 @@
+package com.cdoan.dogs.view
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.cdoan.dogs.R
+import com.cdoan.dogs.model.DogBreed
+import kotlinx.android.synthetic.main.fragment_detail.view.*
+import kotlinx.android.synthetic.main.item_dog.view.*
+
+class DogListAdapter(private var dogList: List<DogBreed>) : RecyclerView.Adapter<DogListAdapter.DogViewHolder>() {
+
+    class DogViewHolder(var view: View) : RecyclerView.ViewHolder(view)
+
+    fun updateDogList(newDogList: List<DogBreed>) {
+        dogList = newDogList
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DogViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.item_dog, parent)
+        return DogViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: DogViewHolder, position: Int) {
+        holder.view.dogName.text = dogList[position].dogBreed
+        holder.view.lifespan.text = dogList[position].lifespan
+    }
+
+    override fun getItemCount() = dogList.size
+
+}
