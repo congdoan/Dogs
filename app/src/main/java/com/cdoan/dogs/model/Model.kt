aaaca6f360c1976035da2c7998501fb709df7acc
Cong Doan <cong.doan@gmail.com>
2021-07-11 05:08:43 +0700

    Implement Retrofit classes: DogBreed data model updated with @SerializedName, DogsApi interface, DogsApiService class.

--- app/src/main/java/com/cdoan/dogs/model/Model.kt
+++ app/src/main/java/com/cdoan/dogs/model/Model.kt
@@ -1,11 +1,25 @@
 package com.cdoan.dogs.model
 
+import com.google.gson.annotations.SerializedName
+
 data class DogBreed(
+    @SerializedName("id")
     val breedId: String?,
+
+    @SerializedName("name")
     val dogBreed: String?,
+
+    @SerializedName("life_span")
     val lifespan: String?,
+
+    @SerializedName("breed_group")
     val breedGroup: String? = null,
+
+    @SerializedName("bred_for")
     val bredFor: String? = null,
+
     val temperament: String? = null,
+
+    @SerializedName("url")
     val imageUrl: String? = null
 )
