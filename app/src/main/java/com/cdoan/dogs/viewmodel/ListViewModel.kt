3899b52b985d6dc9a7d44e85fb8747b06504cb5f
Cong DOAN (Ingenico Partner) <Cong.DOANCHI@ingenico.com>
2021-07-11 17:32:56 +0700

    Update ListViewModel in order to use DogDatabase to store dogs, retrieved from remote api, in the local database.

--- app/src/main/java/com/cdoan/dogs/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/cdoan/dogs/viewmodel/ListViewModel.kt
@@ -1,16 +1,19 @@
 package com.cdoan.dogs.viewmodel
 
+import android.app.Application
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.cdoan.dogs.model.DogBreed
+import com.cdoan.dogs.model.DogDatabase
 import com.cdoan.dogs.model.DogsApiService
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.observers.DisposableSingleObserver
 import io.reactivex.schedulers.Schedulers
+import kotlinx.coroutines.launch
 
-class ListViewModel : ViewModel() {
+class ListViewModel(application: Application) : BaseViewModel(application) {
 
     sealed class LoadState
     object LoadStateLoading : LoadState()
@@ -33,7 +36,7 @@ class ListViewModel : ViewModel() {
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribeWith(object : DisposableSingleObserver<List<DogBreed>>() {
                     override fun onSuccess(dogs: List<DogBreed>) {
-                        data.value = LoadStateSuccess(dogs)
+                        storeDogsLocally(dogs)
                     }
 
                     override fun onError(error: Throwable) {
@@ -43,6 +46,23 @@ class ListViewModel : ViewModel() {
                     }
                 })
         )
+    }
+
+    private fun storeDogsLocally(dogs: List<DogBreed>) {
+        launch {
+            val dao = DogDatabase(getApplication()).dogDao()
+            dao.deleteAllDogs()
+            val dogIds = dao.insertDogs(*dogs.toTypedArray())
+            for (i in dogs.indices) {
+                dogs[i].uuid = dogIds[i].toInt()
+            }
+
+            dogsRetrieved(dogs)
+        }
+    }
+
+    private fun dogsRetrieved(dogs: List<DogBreed>) {
+        data.value = LoadStateSuccess(dogs)
     }
 
     override fun onCleared() {
