5af35fd61daf37b41229cc5ef64eb2636592e029
Cong Doan <cong.doan@gmail.com>
2021-07-14 06:39:59 +0700

    Set background color for DetailFragment using Palette & Glide libs.

--- app/src/main/java/com/cdoan/dogs/view/DetailFragment.kt
+++ app/src/main/java/com/cdoan/dogs/view/DetailFragment.kt
@@ -1,5 +1,7 @@
 package com.cdoan.dogs.view
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,8 +9,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
+import androidx.palette.graphics.Palette
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.cdoan.dogs.R
 import com.cdoan.dogs.databinding.FragmentDetailBinding
+import com.cdoan.dogs.model.DogPalette
 import com.cdoan.dogs.util.loadImage
 import com.cdoan.dogs.viewmodel.DetailViewModel
 import kotlinx.android.synthetic.main.fragment_detail.*
@@ -63,7 +70,28 @@ class DetailFragment : Fragment() {
     private fun observeDetailViewModel() {
         detailViewModel.data.observe(this) {
             dataBinding.dog = it
+
+            it.imageUrl?.let { url ->
+                setBackgroundColor(url)
+            }
         }
+    }
+
+    private fun setBackgroundColor(url: String) {
+        Glide.with(this)
+            .asBitmap()
+            .load(url)
+            .into(object : CustomTarget<Bitmap>() {
+                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                    Palette.from(resource)
+                        .generate { palette ->
+                            val intColor = palette?.vibrantSwatch?.rgb ?: 0
+                            dataBinding.palette = DogPalette(intColor)
+                        }
+                }
+
+                override fun onLoadCleared(placeholder: Drawable?) {}
+            })
     }
 
     companion object {
